c251513d90834fc8b91089f9ba1be0cf8029df3e
TiamatXu <tiamat.xu@icloud.com>
2026-01-20 18:48:12 +0800

    feat: initialize project structure with Gradle and Spring Boot applications

---
+++ task-agent/src/main/java/com/example/distributedtask/TaskAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributedtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskAgentApplication.class, args);
+    }
+
+}
